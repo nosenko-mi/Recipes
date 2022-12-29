3d481de3f0c00af55d9135bf077bdc35c913c26b
nosenko-mi <nosenkomi02@gmail.com>
2022-12-29 18:07:19 +0200

    [feat] taking photo works.

--- app/src/main/java/com/ltl/recipes/ui/NewRecipeFragment.kt
+++ app/src/main/java/com/ltl/recipes/ui/NewRecipeFragment.kt
@@ -1,12 +1,24 @@
 package com.ltl.recipes.ui
 
+import android.Manifest
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -41,28 +53,101 @@ class NewRecipeFragment : Fragment() {
     private fun showBottomSheetDialog() {
 
         val bottomSheetDialog = BottomSheetDialog(requireContext())
-        bottomSheetDialog.setContentView(R.layout.add_img_bottom_sheet_dialog);
+        bottomSheetDialog.setContentView(R.layout.add_img_bottom_sheet_dialog)
         val camera = bottomSheetDialog.findViewById<LinearLayout>(R.id.cameraLayout)
         val gallery = bottomSheetDialog.findViewById<LinearLayout>(R.id.galleryLayout)
 
         if (camera != null && gallery!= null) {
             camera.setOnClickListener{
-//                camera intent
-                Toast.makeText(context, "camera", Toast.LENGTH_SHORT).show()
-                bottomSheetDialog.dismiss();
+                takePhotoSequence()
+                bottomSheetDialog.dismiss()
             }
             gallery.setOnClickListener{
 //                gallery intent
                 Toast.makeText(context, "gallery", Toast.LENGTH_SHORT).show()
-                bottomSheetDialog.dismiss();
+                bottomSheetDialog.dismiss()
             }
         }
 
         bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
 
-        bottomSheetDialog.show();
+        bottomSheetDialog.show()
 
     }
 
+    private fun takePhotoSequence(){
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults:IntArray) {
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(context,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+//                finish()
+            }
+        }
+    }
+
+
+    private fun startCamera(){
+        dispatchTakePictureIntent()
+    }
+
+    private var cameraLauncher = registerForActivityResult<Intent, ActivityResult>(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // BitMap is data structure of image file which store the image in memory
+            val photo = result.data!!.extras!!["data"] as Bitmap?
+            // Set the image in imageview for display
+            recipeImg.setImageBitmap(photo)
+        }
+        else {
+            Toast.makeText(context, "Error while taking photo", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            cameraLauncher.launch(takePictureIntent)
+
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
 
 }
