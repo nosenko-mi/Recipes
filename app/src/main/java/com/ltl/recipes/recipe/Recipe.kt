abee5d1c077dfdddc72c3eea33071837b501b781
nosenko-mi <nosenkomi02@gmail.com>
2023-01-21 17:46:02 +0200

    feat: class -> data class, implement Validatable

--- app/src/main/java/com/ltl/recipes/recipe/Recipe.kt
+++ app/src/main/java/com/ltl/recipes/recipe/Recipe.kt
@@ -2,24 +2,30 @@ package com.ltl.recipes.recipe
 
 import com.google.gson.Gson
 import com.ltl.recipes.ingredient.Ingredient
+import com.ltl.recipes.utils.Validatable
 import com.ltl.recipes.utils.Writable
 import org.json.JSONObject
 
 var recipeList = mutableListOf<Recipe>()
 
-class Recipe (
+data class Recipe (
     var coverImg: Int = 0,
-    var title: String = "abstract",
-    var description: String = "abstract",
+    var imgRef: String = "name.jpg",
+    var title: String = "",
+    var description: String = "",
     var isPublic: Boolean = false,
     var servingsNum: Int = 1,
-    var ingredients: List<Ingredient> = listOf(Ingredient("i1", 1f), Ingredient("i2", 2f)),
-    var steps: List<String> = listOf("step1", "step2", "step3"),
+    var ingredients: List<Ingredient> = emptyList(),
+    var steps: String = "How to cook: \nFirstly, ... \nSecondly, ,,,\nFinally, ...",
     var tags: List<String> = emptyList(),
     val id: Int? = recipeList.size
         )
-    : Writable
+    : Writable, Validatable
 {
+
+    override fun isValid(): Boolean {
+        return title != "" && ingredients.isNotEmpty()
+    }
 
     override fun toJson(): JSONObject {
         val gson = Gson()
