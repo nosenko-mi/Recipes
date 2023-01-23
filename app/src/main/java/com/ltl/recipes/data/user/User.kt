0cf6c18771d602c6823822ae11896432fd6793be
nosenko-mi <nosenkomi02@gmail.com>
2023-01-23 15:49:20 +0200

    feat: add basic User and UserModel

---
+++ app/src/main/java/com/ltl/recipes/data/user/User.kt
@@ -0,0 +1,8 @@
+package com.ltl.recipes.data.user
+
+data class User(
+    var id: Long,
+    var username: String,
+    var displayName: String,
+    var email: String
+)
---
+++ app/src/main/java/com/ltl/recipes/data/user/UserModel.kt
@@ -0,0 +1,6 @@
+package com.ltl.recipes.data.user
+
+data class UserModel(
+    var displayName: String,
+    var email: String
+)
