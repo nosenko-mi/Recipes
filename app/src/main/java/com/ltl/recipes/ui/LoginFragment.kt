e5f134f57ebad5546ba84f8075012718b6a9fdeb
nosenko-mi <nosenkomi02@gmail.com>
2023-01-28 16:41:47 +0200

    feat: implement Email authentication, adding user to db, little enhancements

--- app/src/main/java/com/ltl/recipes/ui/LoginFragment.kt
+++ app/src/main/java/com/ltl/recipes/ui/LoginFragment.kt
@@ -1,19 +1,20 @@
 package com.ltl.recipes.ui
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.Navigation
+import androidx.navigation.navGraphViewModels
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
@@ -22,18 +23,26 @@ import com.google.firebase.auth.GoogleAuthProvider
 import com.ltl.recipes.AppConfig
 import com.ltl.recipes.R
 import com.ltl.recipes.data.user.UserModel
+import com.ltl.recipes.data.user.UserRegistrationRequest
+import com.ltl.recipes.data.user.UserRepository
+import com.ltl.recipes.data.user.UserViewModel
 import com.ltl.recipes.databinding.FragmentLoginBinding
 import java.util.*
 
 
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(), OnClickListener {
 
     private lateinit var binding: FragmentLoginBinding
 
-    private lateinit var account: GoogleSignInAccount
-    private lateinit var mAuth: FirebaseAuth
+    private val userRepository = UserRepository()
 
-    private var googleSignInLauncher = registerForActivityResult<Intent, ActivityResult>(
+    private lateinit var account: GoogleSignInAccount
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var gso: GoogleSignInOptions
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private val userViewModel: UserViewModel by navGraphViewModels(R.id.nav_graph)
+
+    private var googleSignInLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
@@ -42,7 +51,7 @@ class LoginFragment : Fragment() {
             try {
                 account = task.getResult(ApiException::class.java)
 
-                firebaseAuthWithGoogle(account)
+                authWithGoogle(account)
                 Log.e(TAG, "GoogleSignIn: success")
 
             } catch (e: ApiException) {
@@ -67,34 +76,36 @@ class LoginFragment : Fragment() {
         }
     }
 
-    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+    private fun authWithEmail(user: UserRegistrationRequest){
+        firebaseAuth.signInWithEmailAndPassword(user.email, user.password)
+            .addOnCompleteListener{ task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:success")
+                    val currentUser = UserModel(
+                        firebaseAuth.currentUser?.displayName.toString(),
+                        firebaseAuth.currentUser?.email.toString()
+                    )
+                    updateUiWithUser(currentUser)
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(context, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+
+    private fun authWithGoogle(acct: GoogleSignInAccount) {
         val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
-        mAuth.signInWithCredential(credential)
+        firebaseAuth.signInWithCredential(credential)
             .addOnSuccessListener {
-
-                val userModel = UserModel(
-                    acct.displayName!!,
-                    acct.email!!
-                )
-
+                val userModel = UserModel(acct.displayName!!, acct.email!!)
+                userRepository.addUser(userModel)
                 updateUiWithUser(userModel)
             }
             .addOnFailureListener {
-                Toast.makeText(context, "Authentication failed.",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Authentication: failed.",Toast.LENGTH_SHORT).show()
             }
-    }
-
-    private fun updateUiWithUser(userModel: Any) {
-        goToMainFragment()
-    }
-
-    private fun goToMainFragment() {
-        view?.let { Navigation.findNavController(it).navigate(R.id.loginFragmentToMainFragment) }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -109,38 +120,72 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        mAuth = FirebaseAuth.getInstance()
+        firebaseAuth = FirebaseAuth.getInstance()
 
         // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = mAuth.currentUser
+        val currentUser = firebaseAuth.currentUser
         if (currentUser != null) {
             Log.d(TAG, currentUser.email.toString())
-
             val userModel = UserModel(currentUser.displayName.toString(), currentUser.email.toString())
-
             updateUiWithUser(userModel)
         }
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .requestIdToken(
                 Objects.requireNonNull(AppConfig.getConfigValue(context, "web_client_id"))
                 .toString())
             .build()
 
-
         // Build a GoogleSignInClient with the options specified by gso.
-        val mGoogleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+        mGoogleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
 
-        binding.loginGoogleButton.setOnClickListener {
-            val signInIntent = mGoogleSignInClient.signInIntent
-            googleSignInLauncher.launch(signInIntent)
-        }
+        binding.loginGoogleButton.setOnClickListener(this)
+        binding.loginButton.setOnClickListener(this)
+        binding.registerTextView.setOnClickListener(this)
     }
 
+    private fun updateUiWithUser(userModel: UserModel) {
+        userViewModel.setNewUser(userModel)
+        goToMainFragment()
+    }
 
+    private fun goToMainFragment() {
+        view?.let { Navigation.findNavController(it).navigate(R.id.loginFragmentToMainFragment) }
+    }
+
+    private fun goToRegistrationFragment(){
+        view?.let { Navigation.findNavController(it).navigate(R.id.loginFragmentToRegistrationFragment) }
+    }
 
     companion object {
-        private const val TAG = "Login"
+        private const val TAG = "LoginFragment"
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            binding.loginButton.id -> {
+                val user = UserRegistrationRequest(
+                    binding.emailEditText.text.toString(),
+                    binding.emailEditText.text.toString(),
+                    binding.passwordEditText.text.toString()
+                )
+                if (user.isValid()){
+                    authWithEmail(user)
+                } else{
+//                    show errors
+                    Toast.makeText(context, "Bad credentials", Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, "AuthEmail: bad formatting/pattern")
+                }
+            }
+            binding.loginGoogleButton.id -> {
+                val signInIntent = mGoogleSignInClient.signInIntent
+                googleSignInLauncher.launch(signInIntent)
+            }
+            binding.registerTextView.id -> {
+                goToRegistrationFragment()
+            }
+            else -> Toast.makeText(context, "else", Toast.LENGTH_SHORT).show()
+        }
     }
 }
