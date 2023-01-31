ba65d1557ad448e640e1ed9cf33a340b4e12351e
nosenko-mi <nosenkomi02@gmail.com>
2023-01-31 14:45:35 +0200

    feat: change attributes

--- app/src/main/java/com/ltl/recipes/recipe/Recipe.kt
+++ app/src/main/java/com/ltl/recipes/recipe/Recipe.kt
@@ -1,15 +1,20 @@
 package com.ltl.recipes.recipe
 
+import com.google.firebase.firestore.ServerTimestamp
 import com.google.gson.Gson
 import com.ltl.recipes.ingredient.Ingredient
 import com.ltl.recipes.utils.Validatable
 import com.ltl.recipes.utils.Writable
 import org.json.JSONObject
+import java.util.*
 
 var recipeList = mutableListOf<Recipe>()
 
 data class Recipe (
     var coverImg: Int = 0,
+    var author: String = "",
+    @ServerTimestamp
+    var createdAt: Date? = null,
     var imgRef: String = "name.jpg",
     var title: String = "",
     var description: String = "",
@@ -19,7 +24,7 @@ data class Recipe (
     var steps: String = "How to cook: \nFirstly, ... \nSecondly, ,,,\nFinally, ...",
     var tags: List<String> = emptyList(),
     val id: Int? = recipeList.size
-        )
+)
     : Writable, Validatable
 {
 
