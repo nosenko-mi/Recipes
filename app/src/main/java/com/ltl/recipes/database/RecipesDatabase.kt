1dfd1df1964a8be689137c4d226066a060a0e649
nosenko-mi <nosenkomi02@gmail.com>
2023-02-06 13:06:07 +0200

    feat: add RecipeDatabase for local db

---
+++ app/src/main/java/com/ltl/recipes/database/RecipesDatabase.kt
@@ -0,0 +1,33 @@
+package com.ltl.recipes.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.ltl.recipes.database.recipe.RecipeDao
+import com.ltl.recipes.database.recipe.RecipeEntity
+
+@Database(entities = [RecipeEntity::class], version = 3)
+@TypeConverters(Converters::class)
+abstract  class RecipesDatabase: RoomDatabase(){
+
+    abstract fun recipeDao(): RecipeDao
+
+}
+
+private lateinit var INSTANCE: RecipesDatabase
+
+fun getInstance(context: Context): RecipesDatabase{
+    synchronized(RecipesDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                RecipesDatabase::class.java,
+                "recipes_db")
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
+    return INSTANCE
+}
