20a44adf745d302ddc776edb09530eb63ebf1861
nosenko-mi <nosenkomi02@gmail.com>
2023-02-06 13:05:35 +0200

    feat: add RecipeEntity.kt

---
+++ app/src/main/java/com/ltl/recipes/database/recipe/RecipeEntity.kt
@@ -0,0 +1,33 @@
+package com.ltl.recipes.database.recipe
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.ltl.recipes.data.recipe.Recipe
+import com.ltl.recipes.ingredient.Ingredient
+import java.util.*
+
+@Entity(tableName = "recipe_table")
+data class RecipeEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+    var coverImg: Int = 0,
+    var imgRef: String = "name.jpg",
+    var author: String = "",
+    var createdAt: Date? = null,
+    var title: String = "",
+    var description: String = "",
+    var servingsNum: Int = 1,
+    var ingredients: List<Ingredient> = emptyList(),
+    var steps: String = "",
+    var isPublic: Boolean = false,
+//    var tags: List<String> = emptyList(),
+)
+
+fun List<RecipeEntity>.asDomainModel(): List<Recipe> {
+    return map {
+        Recipe(
+            it.coverImg, it.author, it.createdAt, it.imgRef, it.title, it.description,
+            it.isPublic, it.servingsNum, it.ingredients, it.steps, emptyList(), it.id
+        )
+    }
+}
