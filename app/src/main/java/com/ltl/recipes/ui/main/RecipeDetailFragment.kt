85c9331ef304df930a5cc088bb52e903e0743245
nosenko-mi <nosenkomi02@gmail.com>
2023-02-25 16:56:03 +0200

    feat: add recyclerView for list of ingredients

--- app/src/main/java/com/ltl/recipes/ui/main/RecipeDetailFragment.kt
+++ app/src/main/java/com/ltl/recipes/ui/main/RecipeDetailFragment.kt
@@ -1,60 +1,98 @@
 package com.ltl.recipes.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toDrawable
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.ltl.recipes.R
+import com.ltl.recipes.data.recipe.Recipe
+import com.ltl.recipes.databinding.FragmentRecipeDetailBinding
+import com.ltl.recipes.ingredient.Ingredient
+import com.ltl.recipes.ingredient.IngredientAccessType
+import com.ltl.recipes.ingredient.IngredientRecycleViewAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RecipeDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RecipeDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentRecipeDetailBinding
+    private val args: RecipeDetailFragmentArgs by navArgs()
+
+    private lateinit var ingredientRecycleViewAdapter: IngredientRecycleViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_detail, container, false)
+    ): View {
+        binding = FragmentRecipeDetailBinding.inflate(inflater, container, false)
+
+        val layoutManager = LinearLayoutManager(context)
+        binding.ingredientRecycleView.layoutManager = layoutManager
+        binding.ingredientRecycleView.itemAnimator = DefaultItemAnimator()
+        ingredientRecycleViewAdapter = IngredientRecycleViewAdapter(
+            args.recipeAttr.ingredients.toMutableList(),
+            IngredientAccessType.READ,
+            ::deleteIngredient,
+            ::editIngredient)
+
+        binding.ingredientRecycleView.adapter = ingredientRecycleViewAdapter
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        populateRecipe(args.recipeAttr)
+    }
+
+    private fun populateRecipe(recipe: Recipe){
+        binding.recipeTitle.text = recipe.title
+        binding.descriptionText.text = recipe.description
+        binding.stepsText.text = recipe.steps
+
+
+        loadImg(recipe.imgRef)
+    }
+
+    private fun loadImg(ref: String){
+//        TODO create single db access, add default img path
+        val location = "tests/$ref.jpg"
+        Log.d(TAG, "loadImg: location = $location")
+        val storageReference = FirebaseStorage.getInstance().getReference("tests")
+        // Create a reference to the file to delete
+        val fileRef: StorageReference = storageReference.child(ref)
+
+        Glide.with(requireContext())
+            .load(fileRef)
+            .placeholder(R.drawable.recipe_default)
+            .into(binding.recipeImg)
+
+
+    }
+
+    private fun deleteIngredient(ingredient: Ingredient){
+    }
+
+    private fun editIngredient(ingredient: Ingredient){
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RecipeDetailFragment.
-         */
+        private const val TAG = "RecipeDetailFragment"
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RecipeDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = RecipeDetailFragment()
     }
 }
