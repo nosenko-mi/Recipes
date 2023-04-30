ec39b298442db57fba55e240b9efff918aa4d02b
nosenko-mi <nosenkomi02@gmail.com>
2023-04-30 19:52:52 +0300

    feat: add empty NewRecipeViewModel.kt

---
+++ app/src/main/java/com/ltl/recipes/viewmodels/NewRecipeViewModel.kt
@@ -0,0 +1,21 @@
+package com.ltl.recipes.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class NewRecipeViewModel: ViewModel() {
+    // Add states for inputs
+
+    // Add RecipeRepository (+ remove it from NewRecipeFragment)
+
+    // What to do with ingredients? Mb leave it as is and use viewmodel for it?
+
+    fun getRecipe(){}
+
+    fun setRecipe(){}
+
+    fun checkInputs(){}
+
+    fun addRecipe(){}
+
+    // ... add another logics
+}
