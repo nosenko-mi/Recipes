9f7433e1691e39eba2a763df78018054add72af7
nosenko-mi <nosenkomi02@gmail.com>
2023-06-01 16:49:10 +0300

    new: add files with constants

---
+++ app/src/main/java/com/ltl/recipes/constants/AppConstants.kt
@@ -0,0 +1,7 @@
+package com.ltl.recipes.constants
+
+class AppConstants {
+    companion object{
+        const val defaultImgRef = "default.jpg"
+    }
+}
---
+++ app/src/main/java/com/ltl/recipes/constants/FirebaseConstants.kt
@@ -0,0 +1,15 @@
+package com.ltl.recipes.constants
+
+class FirebaseConstants {
+
+    companion object {
+        const val StorageBaseUrlTest = "tests/"
+        const val StorageBaseUrl = "imgs/"
+
+        const val FirestoreRecipesCollectionTest = "recipes-test"
+        const val FirestoreRecipesCollection = "recipes"
+    }
+
+
+
+}
