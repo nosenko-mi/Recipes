a221e4aeb366b34edf8dc879d1b6571b86c5a991
nosenko-mi <nosenkomi02@gmail.com>
2023-06-29 18:43:07 +0300

    livedata -> stateflow

--- app/src/main/java/com/ltl/recipes/data/user/UserViewModel.kt
+++ app/src/main/java/com/ltl/recipes/data/user/UserViewModel.kt
@@ -3,22 +3,30 @@ package com.ltl.recipes.data.user
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class UserViewModel: ViewModel() {
 
-    private val userRepository = UserRepository()
-    private var user = MutableLiveData<UserModel>()
+    private val _user = MutableStateFlow<UserModel>(UserModel())
+    val currentUser = _user.asStateFlow()
+    private var user = MutableLiveData<UserModel>(UserModel())
 
     fun setNewUser(newUser: UserModel) {
         user.value = newUser
+        _user.value = newUser
     }
 
-    fun getCurrentUser(): LiveData<UserModel>{
+    fun getCurrentUser(): LiveData<UserModel?>{
         return user
     }
 
     fun getEmail(): String{
         return user.value?.email.toString()
+    }
+
+    fun signOut() {
+        // authClient.signOut()
     }
 
 }
