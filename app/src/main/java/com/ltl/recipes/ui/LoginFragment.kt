ee1e3d15e9eb05c9c4600c3a1e4ea34a42eeaa0d
nosenko-mi <nosenkomi02@gmail.com>
2023-06-29 15:40:20 +0300

    refactor: remove unused code

--- app/src/main/java/com/ltl/recipes/ui/LoginFragment.kt
+++ app/src/main/java/com/ltl/recipes/ui/LoginFragment.kt
@@ -15,18 +15,9 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.navigation.navGraphViewModels
 import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 import com.ltl.recipes.R
 import com.ltl.recipes.data.user.UserModel
 import com.ltl.recipes.data.user.UserRegistrationRequest
-import com.ltl.recipes.data.user.UserRepository
 import com.ltl.recipes.data.user.UserViewModel
 import com.ltl.recipes.databinding.FragmentLoginBinding
 import com.ltl.recipes.firebase.sign_in.FirebaseAuthUiClient
@@ -39,12 +30,6 @@ class LoginFragment : Fragment(), OnClickListener {
 
     private lateinit var binding: FragmentLoginBinding
 
-    private val userRepository = UserRepository()
-
-    private lateinit var account: GoogleSignInAccount
-    private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var gso: GoogleSignInOptions
-    private lateinit var mGoogleSignInClient: GoogleSignInClient
     private val userViewModel: UserViewModel by navGraphViewModels(R.id.nav_graph)
     private val signInViewModel: SignInViewModel by navGraphViewModels(R.id.nav_graph)
 
@@ -53,25 +38,6 @@ class LoginFragment : Fragment(), OnClickListener {
             context = requireContext(),
             oneTapClient = Identity.getSignInClient(requireContext())
         )
-    }
-
-    private var googleSignInLauncher = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val intent = result.data
-            val task = GoogleSignIn.getSignedInAccountFromIntent(intent)
-            try {
-                account = task.getResult(ApiException::class.java)
-
-                authWithGoogle(account)
-                Log.e(TAG, "GoogleSignIn: success")
-
-            } catch (e: ApiException) {
-                Log.e(TAG, "GoogleSignIn: error $e")
-            }
-            handleSignInResult(task)
-        }
     }
 
     private var googleAuthLauncher = registerForActivityResult(
@@ -85,55 +51,6 @@ class LoginFragment : Fragment(), OnClickListener {
                 signInViewModel.onSignInResult(signInResult)
             }
         }
-    }
-
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            account = completedTask.getResult(ApiException::class.java)
-            // Signed in successfully, change activity.
-            val userModel = UserModel(
-                account.displayName!!,
-                account.email!!
-            )
-
-            updateUiWithUser(userModel)
-        } catch (e: ApiException) {
-            Log.e(TAG, "GoogleSignIn: error $e")
-        }
-    }
-
-    private fun authWithEmail(user: UserRegistrationRequest) {
-        firebaseAuth.signInWithEmailAndPassword(user.email, user.password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Log.d(TAG, "signInWithEmail:success")
-                    val currentUser = UserModel(
-                        firebaseAuth.currentUser?.displayName.toString(),
-                        firebaseAuth.currentUser?.email.toString()
-                    )
-                    updateUiWithUser(currentUser)
-                } else {
-                    Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        context, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-
-    }
-
-    private fun authWithGoogle(acct: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
-        firebaseAuth.signInWithCredential(credential)
-            .addOnSuccessListener {
-                val userModel = UserModel(acct.displayName!!, acct.email!!)
-                userRepository.addUser(userModel)
-                updateUiWithUser(userModel)
-            }
-            .addOnFailureListener {
-                Toast.makeText(context, "Authentication: failed.", Toast.LENGTH_SHORT).show()
-            }
     }
 
     override fun onCreateView(
@@ -167,26 +84,6 @@ class LoginFragment : Fragment(), OnClickListener {
                 }
             }
         }
-//        firebaseAuth = FirebaseAuth.getInstance()
-//
-//        // Check if user is signed in (non-null) and update UI accordingly.
-//        val currentUser = firebaseAuth.currentUser
-//        if (currentUser != null) {
-//            Log.d(TAG, currentUser.email.toString())
-//            val userModel = UserModel(currentUser.displayName.toString(), currentUser.email.toString())
-//            updateUiWithUser(userModel)
-//        }
-//
-//        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//            .requestEmail()
-//            .requestIdToken(
-//                Objects.requireNonNull(AppConfig.getConfigValue(context, "web_client_id"))
-//                .toString())
-//            .build()
-//
-//        // Build a GoogleSignInClient with the options specified by gso.
-//        mGoogleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
-//
         binding.loginGoogleButton.setOnClickListener(this)
         binding.loginButton.setOnClickListener(this)
         binding.registerTextView.setOnClickListener(this)
@@ -242,8 +139,6 @@ class LoginFragment : Fragment(), OnClickListener {
                         ).build()
                     )
                 }
-//                val signInIntent = mGoogleSignInClient.signInIntent
-//                googleSignInLauncher.launch(signInIntent)
             }
             binding.registerTextView.id -> {
                 goToRegistrationFragment()
