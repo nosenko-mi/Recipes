ace81077d08a1540616808fa4c227d013476056e
nosenko-mi <nosenkomi02@gmail.com>
2023-08-14 12:07:09 +0300

    update compose theme

--- app/src/main/java/com/ltl/recipes/ui/compose/user_profile/ui/theme/Shape.kt
+++ app/src/main/java/com/ltl/recipes/ui/compose/user_profile/ui/theme/Shape.kt
@@ -1,7 +1,7 @@
 package com.ltl.recipes.ui.compose.user_profile.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
+import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
