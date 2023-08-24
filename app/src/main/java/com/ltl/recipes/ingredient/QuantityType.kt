0b4d37d017eeb908513525bf8269b1e16926a747
nosenko-mi <nosenkomi02@gmail.com>
2023-08-24 13:23:20 +0300

    add GLASS type

--- app/src/main/java/com/ltl/recipes/ingredient/QuantityType.kt
+++ app/src/main/java/com/ltl/recipes/ingredient/QuantityType.kt
@@ -1,6 +1,8 @@
 package com.ltl.recipes.ingredient
 
+import android.app.Application
 import com.google.gson.Gson
+import com.ltl.recipes.R
 import com.ltl.recipes.utils.SpinnerPosition
 import com.ltl.recipes.utils.Writable
 import org.json.JSONObject
@@ -60,9 +62,23 @@ enum class QuantityType(val type: String): Writable, SpinnerPosition, java.io.Se
         override fun toSpinnerPosition(): Int{
             return 4
         }
+    },
+    GLASS("Glass") {
+        override fun toJson(): JSONObject {
+            val gson = Gson()
+            val jsonType: String = gson.toJson(this)
+            return JSONObject(jsonType)
+        }
+
+        override fun toSpinnerPosition(): Int{
+            return 4
+        }
     };
 
     override fun toString(): String {
         return type
     }
 }
+
+
+
